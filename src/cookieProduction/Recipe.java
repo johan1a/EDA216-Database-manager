151b39c449dbfea2fb4d8c54d506c4f9f0958a30
Johan Andersson <johan.peter.andersson@gmail.com>
2014-03-26 12:35:48 +0100

    implemented functionality for adding pallets

--- src/cookieProduction/Recipe.java
+++ src/cookieProduction/Recipe.java
@@ -3,5 +3,10 @@ package cookieProduction;
 import java.util.HashMap;
 
 public class Recipe {
-	HashMap<String,Integer> ingredients; // Ingredient type as key, amount as value
+	HashMap<String, Integer> ingredients; // Ingredient type as key, amount as
+											// value
+
+	public void addIngredientAmount(String productName, int amount) {
+		ingredients.put(productName, amount);
+	}
 }
--- src/database/Database.java
+++ src/database/Database.java
@@ -1,12 +1,16 @@
 package database;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import cookieProduction.Order;
+import cookieProduction.Recipe;
 
 @SuppressWarnings({ "unused", "static-method" })
 public class Database {
@@ -51,38 +55,134 @@ public class Database {
 	 * accordingly.
 	 */
 	public String registerProducedPallets(String productName, String orderID,
-			String productionDate, String productionTime, int amount) {
+			String productionDate, String productionTime, int nbrPallets) {
+		String result = "";
+		try {
+			conn.setAutoCommit(false);
+
+			int insertResult, updateIngredientsResult;
+			// TODO behöver kanske kolla resultatkoderna...
+			for (int i = 0; i < nbrPallets; i++) {
+				insertResult = insertNewPallet(productName, orderID,
+						productionDate, productionTime);
+
+				decrementProductIngredients(productName);
+			}
+
+			conn.commit();
+			result = "Successfully inserted " + nbrPallets + " of "
+					+ " product type " + productName + " into the database.";
+		} catch (SQLException e) {
+			e.printStackTrace();
+			rollback();
+			result = "An error occured. Insertion unsuccesful.";
+		}
+
+		return result;
+	}
+
+	@SuppressWarnings("resource")
+	private int insertNewPallet(String productName, String orderID,
+			String productionDate, String productionTime) throws SQLException {
+
 		String statement;
 		PreparedStatement prepStatement = null;
+
+		/* TODO hämta delivery date osv från databasen med hjälp av orderID */
+		String delivDate = "";
+		String delivTime = "";
+		int result = 0;
+
 		try {
-
-			// minska ingrediensmängd osv
-
+			/* TODO behövs semikolon i SQL-satsen? */
 			statement = "insert into pallets "
 					+ "(cookieName,orderID,productionDate,"
 					+ "productionTime,delivDate,deliveryTime)"
 					+ " values(?,?,?,?,?,?)";
+
 			prepStatement = conn.prepareStatement(statement);
 
-			prepStatement.setString(1, "blabla");
-			conn.commit();
+			prepStatement.setString(1, productName);
+
+			/*
+			 * TODO kanske borde ändra till setInt vid orderID
+			 */
+			prepStatement.setString(2, orderID);
+			prepStatement.setString(3, productionDate);
+			prepStatement.setString(4, productionTime);
+			prepStatement.setString(5, delivDate);
+			prepStatement.setString(6, delivTime);
+
+			result = prepStatement.executeUpdate();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
-			try {
-				conn.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-		} finally {
-			try {
-				if (prepStatement != null) {
-					prepStatement.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			close(prepStatement);
+			throw new SQLException();
 		}
-		return "";
+		close(prepStatement);
+		return result;
+	}
+
+	/*
+	 * Decrements the ingredient storage with the amount it takes to produce a
+	 * pallet of the given product.
+	 * 
+	 * TODO stämmer inte med databasen, ändra table ingredients?
+	 */
+	@SuppressWarnings("resource")
+	private void decrementProductIngredients(String productName)
+			throws SQLException {
+		HashMap<String, Float> ingredientAmounts = getIngredientAmounts(productName);
+		PreparedStatement prepStatement = null;
+
+		/* <Ingredient, amount to decrement> */
+		String statement = "update ingredients" + "set amount=amount-?"
+				+ "where ingredientName = ?";
+		try {
+			prepStatement = conn.prepareStatement(statement);
+			for (Map.Entry<String, Float> entry : ingredientAmounts.entrySet()) {
+				prepStatement.setFloat(1, entry.getValue());
+				prepStatement.setString(2, entry.getKey());
+			}
+			prepStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			close(prepStatement);
+			throw new SQLException();
+		}
+		close(prepStatement);
+	}
+
+	/* Returns the recipe for the given product. */
+	@SuppressWarnings("resource")
+	private HashMap<String, Float> getIngredientAmounts(String productName) throws SQLException {
+		HashMap<String, Float> ingredients = new HashMap<String, Float>();
+		PreparedStatement prepStatement = null;
+		ResultSet rs = null;
+		try {
+			String statement = "select from CookieIngredients where cookieName = ?";
+			prepStatement = conn.prepareStatement(statement);
+			prepStatement.setString(1, productName);
+			rs = prepStatement.executeQuery();
+
+			String ingredient;
+			float amount;
+			while (!rs.isLast()) {
+				rs.next();
+				ingredients.put(rs.getString("ingredientName"),
+						rs.getFloat("amount"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			close(prepStatement);
+			close(rs);
+			throw new SQLException();
+		}
+		close(prepStatement);
+		close(rs);
+
+		return ingredients;
 	}
 
 	/* Registers any number of delivered Pallets. */
@@ -188,16 +288,30 @@ public class Database {
 		return 0;
 	}
 
-	/*
-	 * Returns the amount of the given ingredient left in the ingredient
-	 * storage.
-	 */
-	public int decrementIngredientAmount(String ingredient, int amount) {
-		return 0;
+	/* Closes the given PreparedStatement */
+	private void close(PreparedStatement prepStatement) {
+		try {
+			if (prepStatement != null) {
+				prepStatement.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-	/* Returns the recipe for the given product. */
-	public String getRecipe(String product) {
-		return "";
+	private void close(ResultSet rs) {
+		try {
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void rollback() {
+		try {
+			conn.rollback();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
